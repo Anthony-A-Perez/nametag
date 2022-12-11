1ebc2d673bdb08ef9c8d2cd00207d0d54423af77
Anthony-A-Perez <ap.erez@me.com>
2022-12-10 16:44:37 -0800

    wow

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // grab DOM elements
 
 // set event listeners
+const booger = document.getElementById('nametag');
+const nameDisplay = document.getElementById('name-display');
+const nameInput = document.getElementById('name-input');
+const button = document.getElementById('update-button');
+button.addEventListener('click'), () => {
+        console.log('i am clicking the button');
+        console.log(nameDisplay);
+
+        nameDisplay.textContent = nameInput.value;
+        nameDisplay.style.color = 'red';
+        booger.style.backgroundColor = colorInput.value;
+});
